f007240e593ba3612f6a1bfb3602540cb44c6192
ohexus <moskalenko.taras.d@gmail.com>
2024-03-09 22:46:01 +0200

    Update App styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,12 @@
+import { Box, Container } from '@mui/material';
 import Router from '../router';
 
 const App = () => (
-  <div className="app">
-    <Router />
-  </div>
+  <Container maxWidth={false} disableGutters>
+    <Box sx={{ bgcolor: '#fbfbfb', height: '100vh' }}>
+      <Router />
+    </Box>
+  </Container>
 );
 
 export default App;
