773dbc78470b07f3be81a8d52a9fdc262c7b9d87
ohexus <moskalenko.taras.d@gmail.com>
2024-03-09 17:31:50 +0200

    Create "layouts" module and RootLayout component

---
+++ src/layouts/RootLayout/RootLayout.tsx
@@ -0,0 +1,14 @@
+import { NavBar } from '../../ui';
+
+export interface RootLayoutProps {
+  children?: React.ReactNode;
+}
+
+const RootLayout: React.FC<RootLayoutProps> = ({ children }) => (
+  <div className="root">
+    <NavBar title='TODO: Dynamic Title' />
+    {children}
+  </div>
+);
+
+export default RootLayout;
