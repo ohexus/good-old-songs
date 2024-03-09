8bd70bde5b2d85e776ec7bd1a1ba418bbf9ea507
ohexus <moskalenko.taras.d@gmail.com>
2024-03-10 00:16:14 +0200

    Add "paths" to tsconfig and aliases to webpack config to support absolute imports

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  webpack: function (config, env) {
+    return {
+      ...config,
+      resolve: {
+        alias: {
+          '@/features': path.resolve(__dirname, './src/features'),
+          '@/layouts': path.resolve(__dirname, './src/layouts'),
+          '@/pages': path.resolve(__dirname, './src/pages'),
+          '@/router': path.resolve(__dirname, './src/router'),
+          '@/services': path.resolve(__dirname, './src/services'),
+          '@/types': path.resolve(__dirname, './src/types'),
+          '@/ui': path.resolve(__dirname, './src/ui'),
+        },
+        extensions: ['.js', '.ts', '.tsx'],
+      },
+    };
+  },
+};
