83dc9351e412797bf87325dd1430a4d6f7b7f507
ohexus <moskalenko.taras.d@gmail.com>
2024-03-09 22:50:47 +0200

    Update Artist page and path in order to use ID

--- src/pages/Artist/Artist.tsx
+++ src/pages/Artist/Artist.tsx
@@ -1,7 +1,14 @@
-const Artist = () => (
-  <div>
-    <h1>Artist</h1>
-  </div>
-);
+import { useParams } from 'react-router-dom';
+
+const Artist = () => {
+  const { artistId } = useParams();
+
+  return (
+    <div>
+      <h1>Artist</h1>
+      <p>ID: {artistId}</p>
+    </div>
+  );
+};
 
 export default Artist;
