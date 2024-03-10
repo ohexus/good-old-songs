2632e33750209f617c7762426dcfe37d8523352e
ohexus <moskalenko.taras.d@gmail.com>
2024-03-11 00:35:03 +0200

    Apply ArtistProfile to Artist page

--- src/pages/Artist/Artist.tsx
+++ src/pages/Artist/Artist.tsx
@@ -1,14 +1,14 @@
+import { ArtistProfile } from '@/features';
 import { useParams } from 'react-router-dom';
 
 const Artist = () => {
   const { artistId } = useParams();
 
-  return (
-    <div>
-      <h1>Artist</h1>
-      <p>ID: {artistId}</p>
-    </div>
-  );
+  if (!artistId) {
+    throw new Error('No artist ID specified');
+  }
+
+  return <ArtistProfile id={artistId} />;
 };
 
 export default Artist;
