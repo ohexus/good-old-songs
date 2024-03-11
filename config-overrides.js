94b1fe55d9159c9f38e3564ce38242691b2fb6ff
ohexus <moskalenko.taras.d@gmail.com>
2024-03-11 11:08:11 +0200

    Add "app" to paths

--- config-overrides.js
+++ config-overrides.js
@@ -6,6 +6,7 @@ module.exports = {
       ...config,
       resolve: {
         alias: {
+          '@/app': path.resolve(__dirname, './src/app'),
           '@/features': path.resolve(__dirname, './src/features'),
           '@/layouts': path.resolve(__dirname, './src/layouts'),
           '@/pages': path.resolve(__dirname, './src/pages'),
