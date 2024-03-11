100243f423450e57617496b0209f7a85cc5e72f1
ohexus <moskalenko.taras.d@gmail.com>
2024-03-11 23:37:46 +0200

    Change SongCard layout and styles to make it gorizontal

--- src/ui/cards/SongCard/SongCard.tsx
+++ src/ui/cards/SongCard/SongCard.tsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import {
+  Box,
   Card,
   CardActions,
   CardContent,
@@ -11,6 +13,7 @@ import {
   Favorite as FavoriteIcon,
   FavoriteBorder as FavoriteBorderIcon,
 } from '@mui/icons-material';
+import { RoutePaths } from '@/router';
 import { Song } from '@/types';
 
 export interface SongCardProps {
@@ -21,12 +24,12 @@ export interface SongCardProps {
 }
 
 const SongCard: React.FC<SongCardProps> = ({
-  data: { artistName, cover, duration, id, name },
+  data: { artistId, artistName, cover, duration, id, name },
   addCta,
   removeCta,
   defaultFavorite,
 }) => {
-  const [isFavorite, setIsFavorite] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(defaultFavorite);
 
   const handleFavoriteClick = () => {
     const newIsFavorite = !isFavorite;
@@ -36,50 +39,58 @@ const SongCard: React.FC<SongCardProps> = ({
     newIsFavorite ? addCta(id) : removeCta(id);
   };
 
-  useEffect(() => {
-    setIsFavorite(!!defaultFavorite);
-  }, [defaultFavorite]);
-
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        image={cover}
-        title={name}
-        style={{ objectFit: 'cover' }}
-      />
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="body2"
-          component="h1"
-          title={name}
-          overflow="hidden"
-          textOverflow="ellipsis"
-          whiteSpace="nowrap"
-        >
-          {name}
-        </Typography>
-        <Typography
-          gutterBottom
-          variant="body2"
-          color="text.secondary"
-          title={name}
-          overflow="hidden"
-          textOverflow="ellipsis"
-          whiteSpace="nowrap"
-        >
-          {artistName}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {duration}
-        </Typography>
-      </CardContent>
+    <Card sx={{ display: 'flex', width: '100%' }}>
       <CardActions>
         <IconButton onClick={handleFavoriteClick}>
           {isFavorite ? <FavoriteIcon /> : <FavoriteBorderIcon />}
         </IconButton>
       </CardActions>
+      <CardMedia
+        component="img"
+        height="128"
+        image={cover}
+        title={name}
+        alt={`${cover} song cover`}
+        style={{ objectFit: 'contain', width: 'auto' }}
+      />
+      <CardContent
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          flex: 1,
+          padding: 2,
+          '&:last-child': {
+            padding: 2,
+          },
+        }}
+      >
+        <Box flex={1}>
+          <Typography gutterBottom variant="h5" component="h1" title={name}>
+            {name}
+          </Typography>
+          <Typography
+            variant="h6"
+            component={Link}
+            to={`/${RoutePaths.ARTIST}/${artistId}`}
+            color="text.secondary"
+            title={artistName}
+            sx={{
+              textDecoration: 'none',
+              '&:hover': {
+                textDecoration: 'underline',
+              },
+            }}
+          >
+            {artistName}
+          </Typography>
+        </Box>
+        <Box px={2}>
+          <Typography variant="body1" component="h3" color="text.secondary">
+            {duration}
+          </Typography>
+        </Box>
+      </CardContent>
     </Card>
   );
 };
