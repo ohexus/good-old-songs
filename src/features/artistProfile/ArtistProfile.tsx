570095b3a97a6563b948104dfdb240aaadf540e0
ohexus <moskalenko.taras.d@gmail.com>
2024-03-11 12:46:38 +0200

    Update Artist page using PageLayout component

--- src/features/artistProfile/ArtistProfile.tsx
+++ src/features/artistProfile/ArtistProfile.tsx
@@ -2,7 +2,6 @@ import {
   Avatar,
   Box,
   CircularProgress,
-  Container,
   Typography,
 } from '@mui/material';
 import { useGetArtistByIdQuery } from '@/services/artistsApi';
@@ -15,6 +14,10 @@ export interface ArtistProfileProps {
 const ArtistProfile: React.FC<ArtistProfileProps> = ({ id }) => {
   const { data, error, isLoading } = useGetArtistByIdQuery(id);
 
+  if (isLoading) {
+    return <CircularProgress />;
+  }
+
   if (error) {
     throw error;
   }
@@ -24,34 +27,24 @@ const ArtistProfile: React.FC<ArtistProfileProps> = ({ id }) => {
   }
 
   return (
-    <Container
-      sx={{
-        height: '100%',
-      }}
-    >
-      {isLoading ? (
-        <CircularProgress />
-      ) : (
-        <>
-          <Box display="flex" alignItems="center" gap={4} pt={4} mb={4}>
-            <Avatar
-              src={data.avatar}
-              alt={data.name}
-              sx={{ width: 144, height: 144 }}
-            />
-            <Typography variant="h2" component="h1">
-              {data.name}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h4" component="h2" gutterBottom>
-              Songs
-            </Typography>
-            <Songs artistId={id} totalAmount={data.songsCount} />
-          </Box>
-        </>
-      )}
-    </Container>
+    <>
+      <Box display="flex" alignItems="center" gap={4} pt={4} mb={4}>
+        <Avatar
+          src={data.avatar}
+          alt={data.name}
+          sx={{ width: 144, height: 144 }}
+        />
+        <Typography variant="h2" component="h1">
+          {data.name}
+        </Typography>
+      </Box>
+      <Box>
+        <Typography variant="h4" component="h2" gutterBottom>
+          Songs
+        </Typography>
+        <Songs artistId={id} totalAmount={data.songsCount} />
+      </Box>
+    </>
   );
 };
 
